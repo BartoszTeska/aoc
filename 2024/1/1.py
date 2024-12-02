8523009cd6519007c6ebb7aca1826e5f88b675e7
Bartosz Teska <bartek.teska@gmail.com>
2024-12-02 17:28:22 +0100

    day 1

--- 2024/1/1.py
+++ 2024/1/1.py
@@ -1,3 +1,4 @@
+from collections import Counter
 from functools import reduce
 from typing import List, Tuple
 
@@ -12,6 +13,8 @@ def getData(test=True):
             "3   9",
             "3   3",
         ]
+    with open("test1.txt", "r") as file:
+        return file.readlines()
 
 
 def formatData(data: List[str]):
@@ -31,7 +34,16 @@ def solution1(data):
     return reduce(lambda x, y: x + abs(y[0] - y[1]), zip(l1, l2), 0)
 
 
+def solution2(data):
+    l1 = data[0]
+    l2 = data[1]
+    l2Counter = Counter(l2)
+
+    return sum([x * l2Counter[x] for x in l1])
+
+
 if __name__ == "__main__":
-    d = getData(True)
+    d = getData(False)
     d = formatData(d)
     print(solution1(d))
+    print(solution2(d))
